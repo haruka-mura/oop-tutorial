b5a2f5f1d0e8ab822dc3105c185bc64ab3569d5d
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-08-03 17:33:17 +0900

    Use sanitize for query

--- app/models/article_search_query.rb
+++ app/models/article_search_query.rb
@@ -5,23 +5,19 @@ class ArticleSearchQuery
 
   def articles
     scope = Article.all
-    scope = scope.where("title like?", "%#{keyword}%" )
+    scope = scope.where("title like?", "%#{sanitize_sql_like(keyword)}%" )
     scope
   end
 
   # def keyword
   #   "%#{Article.sanitize_sql_like()}%" unless keyword.blank?
   # end
-  #
-  # def conditions
-  #  result = []
-  #  result << "title like :keyword" if keyword
-  #  result
-  # end
-  #
-  # def values
-  #  { keyword: keyword }
-  # end
-end
 
+  private
+
+    def sanitize_sql_like(text)
+      ActiveRecord::Base.send(:sanitize_sql_like, text)
+    end
+
+end
 # ポイント：責務はquery, articleのActiveRcordRelationを返すのかnilを返すのか, sanitaizeを使う
