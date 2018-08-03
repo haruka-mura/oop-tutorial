bee5064f1d6f912314f9bc851ade18db040d9cb5
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-08-03 18:22:09 +0900

    WIP

--- app/models/article_search_query.rb
+++ app/models/article_search_query.rb
@@ -9,15 +9,11 @@ class ArticleSearchQuery
     scope
   end
 
-  # def keyword
-  #   "%#{Article.sanitize_sql_like()}%" unless keyword.blank?
-  # end
-
   private
 
     def sanitize_sql_like(text)
       ActiveRecord::Base.send(:sanitize_sql_like, text)
     end
-
 end
+
 # ポイント：責務はquery, articleのActiveRcordRelationを返すのかnilを返すのか, sanitaizeを使う
