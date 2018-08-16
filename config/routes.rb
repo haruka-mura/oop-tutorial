fabd8673ac688390deef9ee5ffd7f510cf1d9e08
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-08-16 11:09:11 +0900

    Add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :users
   get 'search', to: 'article_search#search'
 
+  get    'login', to: 'sessions#new'
+  post   'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   mount LetterOpenerWeb::Engine, at: "/status/mail"
   resources :articles_categories
   resources :categories
