d478c143edf907bf459a99ed39baa5745ddfa13a
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-08-16 13:33:33 +0900

    prepare models for tag

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
   has_many :articles_categories, dependent: :destroy
   has_many :categories, through: :articles_categories
+  has_many :articles_tags
+  has_many :tags, through: :articles_tags
 
 end
